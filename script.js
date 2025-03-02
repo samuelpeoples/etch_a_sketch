729c32a515df83b672466c681a178799c53910d2
samuelpeoples <peoples.sam@gmail.com>
2025-03-02 17:02:16 +1100

    Create shading mode to slowly adjust opacity of squares to black

--- script.js
+++ script.js
@@ -7,18 +7,30 @@ const rows = document.querySelectorAll("#sketchRow");
 const resetButton = document.querySelector("#reset");
 
 resetButton.addEventListener("click", () => {
-	// grid.forEach((square) => {
-	// 	square.setAttribute("style", "background-color: white;");
-	// });
     refreshGrid();
 });
 
+//REGULAR MODE
+// grid.forEach((square) => {
+// 	square.addEventListener("mouseenter", () => {
+// 		square.setAttribute("style", "background-color: black;");
+// 	});
+// 	square.addEventListener("mouseleave", () => {
+// 		square.setAttribute("style", "background-color: grey;");
+// 	});
+// });
+
+//SHADE MODE
 grid.forEach((square) => {
 	square.addEventListener("mouseenter", () => {
-		square.setAttribute("style", "background-color: black;");
+		//square.setAttribute("style", "background-color: black;");
+        let currentOpacity = getComputedStyle(square).getPropertyValue("opacity");
+        currentOpacity -= 0.1;
+        square.setAttribute("style", `opacity: ${currentOpacity};`);
 	});
 	square.addEventListener("mouseleave", () => {
-		square.setAttribute("style", "background-color: grey;");
+        //square.setAttribute("style", "background-color: grey;");
+        
 	});
 });
 
@@ -37,7 +49,7 @@ function createGrid(gridHeight, gridWidth) {
 		sketchColumn.classList.add("sketchColumn");
 		sketchContainer.appendChild(sketchRow);
 		sketchRow.appendChild(sketchColumn);
-
+        
 		for (let j = 0; j < gridWidth; j++) {
 			const sketchSquare = document.createElement("div");
 			sketchSquare.classList.add("sketchSquare");
@@ -49,7 +61,14 @@ function createGrid(gridHeight, gridWidth) {
 
 function refreshGrid() {
     let gridHeight = prompt("How High do you want this grid?");
+    while (gridHeight > 100){
+        gridHeight = prompt("How High do you want this grid?");
+    }
     let gridWidth = prompt("How Wide do you want this grid?");
+    while (gridWidth > 100 && typeof gridWidth !== Number){
+        gridWidth = prompt("How Wide do you want this grid?");
+    }
+
     sketchContainer.replaceChildren();
     createGrid(gridHeight, gridWidth);
 }
