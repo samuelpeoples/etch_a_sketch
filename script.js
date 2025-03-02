e321aa2a02ad6312989bfab61b01b314c0d0d5ea
samuelpeoples <peoples.sam@gmail.com>
2025-03-02 16:03:34 +1100

    Added hover highlight, residual shading and gird reset

--- script.js
+++ script.js
@@ -1,16 +1,55 @@
-const sketchContainer = document.querySelector("#sketchContainer");
+//PROGRAM
 
-for (let i = 0; i < 16; i++) {
-	const sketchRow = document.createElement("div");
-	sketchRow.classList.add("sketchRow");
-	const sketchColumn = document.createElement("div");
-	sketchColumn.classList.add("sketchColumn");
-	sketchContainer.appendChild(sketchRow);
-	sketchRow.appendChild(sketchColumn);
-    
-	for (let j = 0; j < 16; j++) {
-		const sketchSquare = document.createElement("div");
-		sketchSquare.classList.add("sketchSquare");
-		sketchColumn.appendChild(sketchSquare);
+createGrid(16, 16);
+const grid = document.querySelectorAll("#square");
+const rows = document.querySelectorAll("#sketchRow");
+
+const resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", () => {
+	// grid.forEach((square) => {
+	// 	square.setAttribute("style", "background-color: white;");
+	// });
+    refreshGrid();
+});
+
+grid.forEach((square) => {
+	square.addEventListener("mouseenter", () => {
+		square.setAttribute("style", "background-color: black;");
+	});
+	square.addEventListener("mouseleave", () => {
+		square.setAttribute("style", "background-color: grey;");
+	});
+});
+
+///////////////////////////////////////////////////////////////////////////////
+
+//LOGIC
+
+function createGrid(gridHeight, gridWidth) {
+	const sketchContainer = document.querySelector("#sketchContainer");
+
+	for (let i = 0; i < gridHeight; i++) {
+		const sketchRow = document.createElement("div");
+		sketchRow.classList.add("sketchRow");
+        sketchRow.id = "sketchRow";
+		const sketchColumn = document.createElement("div");
+		sketchColumn.classList.add("sketchColumn");
+		sketchContainer.appendChild(sketchRow);
+		sketchRow.appendChild(sketchColumn);
+
+		for (let j = 0; j < gridWidth; j++) {
+			const sketchSquare = document.createElement("div");
+			sketchSquare.classList.add("sketchSquare");
+			sketchSquare.id = "square";
+			sketchColumn.appendChild(sketchSquare);
+		}
 	}
+}
+
+function refreshGrid() {
+    let gridHeight = prompt("How High do you want this grid?");
+    let gridWidth = prompt("How Wide do you want this grid?");
+    sketchContainer.replaceChildren();
+    createGrid(gridHeight, gridWidth);
 }
